85fe6cc719726eaa85f5c70624988060b84e2fce
Johannes Link <business@johanneslink.net>
2024-11-05 12:20:55 +0100

    Modified routing test to find two pareto optimal solutions

--- application/src/test/java/org/opentripplanner/raptor/heigit_experiments/RoutingAPITest.java
+++ application/src/test/java/org/opentripplanner/raptor/heigit_experiments/RoutingAPITest.java
@@ -1,15 +1,12 @@
 package org.opentripplanner.raptor.heigit_experiments;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.opentripplanner.raptor._data.transit.TestRoute.route;
 import static org.opentripplanner.raptor._data.transit.TestTripPattern.pattern;
 import static org.opentripplanner.raptor._data.transit.TestTripSchedule.schedule;
-import static org.opentripplanner.raptor.api.request.RaptorProfile.MIN_TRAVEL_DURATION;
 import static org.opentripplanner.raptor.api.request.RaptorProfile.STANDARD;
-import static org.opentripplanner.raptor.moduletests.support.RaptorModuleTestConfig.multiCriteria;
-import static org.opentripplanner.raptor.moduletests.support.RaptorModuleTestConfig.standard;
 
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,7 +18,6 @@ import org.opentripplanner.raptor._data.transit.TestTransitData;
 import org.opentripplanner.raptor._data.transit.TestTripSchedule;
 import org.opentripplanner.raptor.api.request.RaptorRequestBuilder;
 import org.opentripplanner.raptor.configure.RaptorConfig;
-import org.opentripplanner.raptor.moduletests.support.RaptorModuleTestCase;
 
 /**
  * FEATURE UNDER TEST
@@ -50,50 +46,39 @@ public class RoutingAPITest implements RaptorTestConstants {
    */
   @BeforeEach
   void setup() {
-    data.withRoute(
-      route(pattern("R1", STOP_B, STOP_C, STOP_D)).withTimetable(
-        schedule("00:01, 00:03, 00:05"),
-        schedule("00:02, 00:05, 00:06")
-      )
-    );
     requestBuilder
       .searchParams()
       .addAccessPaths(TestAccessEgress.walk(STOP_B, D30s))
       .addEgressPaths(TestAccessEgress.walk(STOP_D, D20s))
+      .maxNumberOfTransfers(3)
       .earliestDepartureTime(T00_01)
-      .latestArrivalTime(T00_10)
+      .latestArrivalTime(T00_30)
       .timetable(true);
-
     // ModuleTestDebugLogging.setupDebugLogging(data, requestBuilder);
   }
 
   @Test
-  @DisplayName("min_travel_duration")
-  void minTravelDuration() {
+  void findTwoParetoOptimalSolutionsWithStandardProfile() {
+    data.withRoute(
+      route(pattern("R1", STOP_B, STOP_C, STOP_D)).withTimetable(
+        schedule("00:01, 00:05, 00:09"),
+        schedule("00:03, 00:06, 00:10")
+      )
+    ).withRoute(
+      route(pattern("R2", STOP_A, STOP_C, STOP_D)).withTimetable(
+        schedule("00:01, 00:07, 00:08")
+      )
+    );
+
     requestBuilder.profile(STANDARD);
-//    requestBuilder.profile(MIN_TRAVEL_DURATION);
     requestBuilder.searchParams().searchOneIterationOnly();
 
     var request = requestBuilder.build();
     var response = raptorService.route(request, data);
 
     assertFalse(response.noConnectionFound());
+    assertEquals(2, response.paths().size());
+
     System.out.println(PathUtils.pathsToString(response));
   }
-
-//  static List<RaptorModuleTestCase> testCases() {
-//    var path = "Walk 30s ~ B ~ BUS R1 0:01 0:05 ~ D ~ Walk 20s [0:00:30 0:05:20 4m50s Tₓ0 C₁940]";
-//    return RaptorModuleTestCase
-//      .of()
-//      .addMinDuration("4m50s", TX_0, T00_00, T00_10)
-//      .add(standard(), PathUtils.withoutCost(path))
-//      .add(multiCriteria(), path)
-//      .build();
-//  }
-//  @ParameterizedTest
-//  @MethodSource("testCases")
-//  void testRaptor(RaptorModuleTestCase testCase) {
-//    assertEquals(testCase.expected(), testCase.run(raptorService, data, requestBuilder));
-
-//  }
 }
