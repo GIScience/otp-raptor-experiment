0c792f4eed3686fb924903340b8122a59629e4fa
Johannes Link <business@johanneslink.net>
2024-11-07 10:20:21 +0100

    Made raptor route request work syntactically

--- application/src/test/java/org/opentripplanner/raptor/heigit_experiments/GtfsReadingAndQueryingTest.java
+++ application/src/test/java/org/opentripplanner/raptor/heigit_experiments/GtfsReadingAndQueryingTest.java
@@ -4,8 +4,10 @@ import static org.opentripplanner.framework.time.TimeUtils.hm2time;
 import static org.opentripplanner.raptor.api.request.RaptorProfile.STANDARD;
 
 import java.time.ZonedDateTime;
+import java.util.BitSet;
 import org.junit.jupiter.api.Test;
 import org.opentripplanner.GtfsTest;
+import org.opentripplanner.model.GenericLocation;
 import org.opentripplanner.raptor.RaptorService;
 import org.opentripplanner.raptor._data.transit.TestAccessEgress;
 import org.opentripplanner.raptor.api.request.RaptorEnvironment;
@@ -15,11 +17,17 @@ import org.opentripplanner.raptor.api.request.RaptorTuningParameters;
 import org.opentripplanner.raptor.configure.RaptorConfig;
 import org.opentripplanner.routing.algorithm.raptoradapter.transit.TransitLayer;
 import org.opentripplanner.routing.algorithm.raptoradapter.transit.TransitTuningParameters;
+import org.opentripplanner.routing.algorithm.raptoradapter.transit.TripPatternForDate;
 import org.opentripplanner.routing.algorithm.raptoradapter.transit.TripSchedule;
 import org.opentripplanner.routing.algorithm.raptoradapter.transit.mappers.TransitLayerMapper;
+import org.opentripplanner.routing.algorithm.raptoradapter.transit.request.BoardAlight;
 import org.opentripplanner.routing.algorithm.raptoradapter.transit.request.RaptorRoutingRequestTransitData;
+import org.opentripplanner.routing.algorithm.raptoradapter.transit.request.TransitDataProviderFilter;
+import org.opentripplanner.routing.api.request.RouteRequest;
 import org.opentripplanner.standalone.config.RouterConfig;
+import org.opentripplanner.transit.model.network.RoutingTripPattern;
 import org.opentripplanner.transit.model.network.grouppriority.TransitGroupPriorityService;
+import org.opentripplanner.transit.model.timetable.TripTimes;
 
 class GtfsReadingAndQueryingTest extends GtfsTest {
 
@@ -75,25 +83,53 @@ class GtfsReadingAndQueryingTest extends GtfsTest {
       .timetable(true)
       .build();
 
-    if (1 == 1) return; // To make test succeed
-    // TODO: Fill in missing data in the following code
+    RouteRequest routeRequest = new RouteRequest();
+    routeRequest.setFrom(GenericLocation.fromStopId("START", "FEED:A", startingStop + ""));
+    routeRequest.setTo(GenericLocation.fromStopId("END", "FEED:C", startingStop + ""));
 
-    var raptorDataProvider = createRequestTransitDataProvider(transitLayer);
+    var raptorDataProvider = createRequestTransitDataProvider(transitLayer, routeRequest);
 
     var result = raptorService.route(request, raptorDataProvider);
 
     System.out.println("result.paths() = " + result.paths());
   }
 
-  private RaptorRoutingRequestTransitData createRequestTransitDataProvider(TransitLayer transitLayer) {
+  private RaptorRoutingRequestTransitData createRequestTransitDataProvider(TransitLayer transitLayer, RouteRequest routeRequest) {
     return new RaptorRoutingRequestTransitData(
       transitLayer,
       TransitGroupPriorityService.empty(),
       ZonedDateTime.now(),
       0,
       0,
-      null, // must be filled with valid TransitDataProviderFilter
-      null // must be filled with valid RouteRequest
+      new TestTransitDataProviderFilter(),
+      routeRequest
     );
+  }
+
+  private static class TestTransitDataProviderFilter implements TransitDataProviderFilter {
+
+    @Override
+    public boolean tripPatternPredicate(TripPatternForDate tripPatternForDate) {
+      return true;
+    }
+
+    @Override
+    public boolean tripTimesPredicate(TripTimes tripTimes, boolean withFilters) {
+      return true;
+    }
+
+    @Override
+    public boolean hasSubModeFilters() {
+      return true;
+    }
+
+    @Override
+    public BitSet filterAvailableStops(
+      RoutingTripPattern tripPattern,
+      BitSet boardingPossible,
+      BoardAlight boardAlight
+    ) {
+      return boardingPossible;
+    }
   }
 }
