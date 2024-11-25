a796dfa88d3f152ea910d44b2c556e1cbdd134d6
Matthias Merdes <matthias.merdes@heigit.org>
2024-11-25 17:15:46 +0100

    better separate data and scripts

--- exp/import_gtfs.sql
+++ exp/import_gtfs.sql
@@ -1,19 +1,19 @@
 
--- read the raw gtfs data from unpacked csv-files
+-- read the raw gtfs data from unpacked csv-files in directory `./gtfs_data`
 -- (without transformation or validation for now)
 
-CREATE TABLE __agency         AS SELECT * FROM read_csv('agency.txt');
-CREATE TABLE __attributions   AS SELECT * FROM read_csv('attributions.txt');
-CREATE TABLE __calendar       AS SELECT * FROM read_csv('calendar.txt');
-CREATE TABLE __calendar_dates AS SELECT * FROM read_csv('calendar_dates.txt');
-CREATE TABLE __feed_info      AS SELECT * FROM read_csv('feed_info.txt');
-CREATE TABLE __frequencies    AS SELECT * FROM read_csv('frequencies.txt');
-CREATE TABLE __routes         AS SELECT * FROM read_csv('routes.txt');
-CREATE TABLE __shapes         AS SELECT * FROM read_csv('shapes.txt');
-CREATE TABLE __stop_times     AS SELECT * FROM read_csv('stop_times.txt');
-CREATE TABLE __stops          AS SELECT * FROM read_csv('stops.txt');
-CREATE TABLE __transfers      AS SELECT * FROM read_csv('transfers.txt');
-CREATE TABLE __trips          AS SELECT * FROM read_csv('trips.txt');
+CREATE TABLE __agency         AS SELECT * FROM read_csv('gtfs_data/agency.txt');
+CREATE TABLE __attributions   AS SELECT * FROM read_csv('gtfs_data/attributions.txt');
+CREATE TABLE __calendar       AS SELECT * FROM read_csv('gtfs_data/calendar.txt');
+CREATE TABLE __calendar_dates AS SELECT * FROM read_csv('gtfs_data/calendar_dates.txt');
+CREATE TABLE __feed_info      AS SELECT * FROM read_csv('gtfs_data/feed_info.txt');
+CREATE TABLE __frequencies    AS SELECT * FROM read_csv('gtfs_data/frequencies.txt');
+CREATE TABLE __routes         AS SELECT * FROM read_csv('gtfs_data/routes.txt');
+CREATE TABLE __shapes         AS SELECT * FROM read_csv('gtfs_data/shapes.txt');
+CREATE TABLE __stop_times     AS SELECT * FROM read_csv('gtfs_data/stop_times.txt');
+CREATE TABLE __stops          AS SELECT * FROM read_csv('gtfs_data/stops.txt');
+CREATE TABLE __transfers      AS SELECT * FROM read_csv('gtfs_data/transfers.txt');
+CREATE TABLE __trips          AS SELECT * FROM read_csv('gtfs_data/trips.txt');
 
 
 
